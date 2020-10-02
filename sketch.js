1401bb1609da852f028897bb1e5cf72ff04127b5
Doodleman360 <julislo123@gmail.com>
2020-10-02 15:15:37 -0600

    Background

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+let img;
+function preload() {
+    img = loadImage('background.jpeg');
+}
+
 function setup() {
     createCanvas(displayWidth, displayHeight);
+    background(220);
+    image(img, 0, 0);
 }
 
 function draw() {
-    background(220);
+
 }
