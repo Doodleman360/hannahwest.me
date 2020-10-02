9bea4440dfe96c2b97d9fdbb193c5b4e51bf1a71
Doodleman360 <julislo123@gmail.com>
2020-10-02 15:42:51 -0600

    Small update

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
-let img;
+let bg;
+let SS_Duck;
+
+
 function preload() {
-    img = loadImage('background.jpeg');
+    bg = loadImage('background.jpeg');
+    //SS_Duck = loadSpriteSheet('duck_sheet.png',798/6,527/4,24);
+
 }
 
 function setup() {
     createCanvas(displayWidth, displayHeight);
     background(220);
-    image(img, 0, 0);
+    image(bg, 0, 0, displayWidth, displayHeight);
+    //image(SS_Duck, 0, 0);
 }
 
 function draw() {
-
 }
