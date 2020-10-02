5fdd074568276482580b06be96925c939771341a
Doodleman360 <julislo123@gmail.com>
2020-10-02 17:05:45 -0600

    Update

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-let bg;
+let bg, cnv;
 var SS_Duck = [];
 
 
 function preload() {
     bg = loadImage('assets/background.jpeg');
-    for (let i = 0; i < 17; i++) {
+    for (let i = 0; i < 19; i++) {
         SS_Duck[i] = loadImage('assets/duck_sheet/tile0'+i+'.png');
     }
     //SS_Duck = loadSpriteSheet('duck_sheet.png',798/6,527/4,24);
@@ -12,14 +12,26 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(displayWidth, displayHeight);
+    cnv = createCanvas(displayWidth, displayHeight);
     background(220);
     image(bg, 0, 0, displayWidth, displayHeight);
-    //image(SS_Duck, 0, 0);
+    imageMode(CENTER);
 }
 
+var index = 4;
+var time = 0;
 function draw() {
-    for (var i=0; i<5; i++) {
-        image(SS_Duck[i], random(width), random(height));
+    time += deltaTime
+    image(bg, displayWidth/2, displayHeight/2, displayWidth, displayHeight);
+    image(SS_Duck[index],mouseX,mouseY)
+    if(time > 200) {
+        time = 0;
+        if (++index > 7) {
+            index = 4;
+        }
     }
+}
+
+function mousePressed(){
+    window.open("https://instagram.com/clear_skies42?igshid=3r0bobxrzoxt", "_self");
 }
